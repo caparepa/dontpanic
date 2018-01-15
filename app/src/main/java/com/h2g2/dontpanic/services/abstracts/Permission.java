78662d7c43fcc2880e57b1ef18a6eecac1d583ed
Christopher Serrano <cserrano@teravisiontech.com>
2018-01-15 10:33:16 -0400

    Package configuration - Add interface/abstract class for permissions

---
+++ app/src/main/java/com/h2g2/dontpanic/services/abstracts/Permission.java
@@ -0,0 +1,9 @@
+package com.h2g2.dontpanic.services.abstracts;
+
+/**
+ * Created by C. Serrano (cserrano@teravisiontech.com)
+ * Teravision Technologies
+ * Date: 2018/01/15
+ */
+public interface Permission {
+}
---
+++ app/src/main/java/com/h2g2/dontpanic/services/interfaces/PermissionBase.java
@@ -0,0 +1,11 @@
+package com.h2g2.dontpanic.services.interfaces;
+
+import com.h2g2.dontpanic.services.abstracts.Permission;
+
+/**
+ * Created by C. Serrano (cserrano@teravisiontech.com)
+ * Teravision Technologies
+ * Date: 2018/01/15
+ */
+public abstract class PermissionBase implements Permission {
+}
