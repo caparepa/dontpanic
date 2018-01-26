9d8df89de3a021788284aacd457729c5c2f6d6e4
Christopher Serrano <cserrano@teravisiontech.com>
2018-01-26 11:50:22 -0400

    Control commit

--- app/src/main/java/com/h2g2/dontpanic/networking/handler/response/AvatarHandler.java
+++ app/src/main/java/com/h2g2/dontpanic/networking/handler/response/AvatarHandler.java
@@ -1,9 +1,39 @@
 package com.h2g2.dontpanic.networking.handler.response;
 
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+import com.h2g2.dontpanic.networking.base.RetroBase;
+import com.h2g2.dontpanic.networking.interfaces.Avatar;
+import com.h2g2.dontpanic.session.SessionUtil;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+
 /**
  * Created by C. Serrano (cserrano@teravisiontech.com)
  * Teravision Technologies
  * Date: 2018/01/26
  */
-public class AvatarHandler {
+public class AvatarHandler extends RetroBase {
+    private Avatar routes;
+
+    public AvatarHandler(AppCompatActivity appCompatActivity) {
+        super(appCompatActivity);
+        routes = retrofit.create(Avatar.class);
+    }
+    public void getAvatars(Callback<ResponseBody> callBack){
+        Call<ResponseBody> call=routes.getAvatars();
+        call.enqueue(callBack);
+    }
+    public void getAvatars(Callback<ResponseBody> callBack, Context context){
+        Call<ResponseBody> call=routes.getAvatars(SessionUtil.getHeaderAuthorization(context));
+        //  Call<ResponseBody> call=routes.getAvatars("Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwcm9maWxlX2lkIjo4MSwidXNlcl9pZCI6OTMsInN1YiI6OTMsImlzcyI6Imh0dHA6Ly9kZXYtdW5pY2VmLjQybWF0ZS5jb20vYXBpL3YxL2FjY291bnQvbG9naW4iLCJpYXQiOjE1MTAzNjI0NDgsImV4cCI6MTUxMDM3Njg0OCwibmJmIjoxNTEwMzYyNDQ4LCJqdGkiOiJ4N3REbzVEOGxNUXZTOFdmIn0.cnGtZRTf93BHjxwvyuicUigUd-ojB7aUXaZT80gDE94");
+        call.enqueue(callBack);
+    }
+    public void getAvatar(Callback<ResponseBody> callBack, String avatar,Context context){
+        Call<ResponseBody> call=routes.getAvatar(SessionUtil.getHeaderAuthorization(context),avatar);
+        call.enqueue(callBack);
+    }
 }
--- app/src/main/java/com/h2g2/dontpanic/networking/handler/response/TrackerHandler.java
+++ app/src/main/java/com/h2g2/dontpanic/networking/handler/response/TrackerHandler.java
@@ -1,9 +1,51 @@
 package com.h2g2.dontpanic.networking.handler.response;
 
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+import com.h2g2.dontpanic.networking.base.RetroBase;
+import com.h2g2.dontpanic.networking.interfaces.Avatar;
+import com.h2g2.dontpanic.networking.interfaces.Tracker;
+import com.h2g2.dontpanic.session.SessionUtil;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+
 /**
  * Created by C. Serrano (cserrano@teravisiontech.com)
  * Teravision Technologies
  * Date: 2018/01/26
  */
-public class TrackerHandler {
+public class TrackerHandler extends RetroBase{
+    private Tracker routes;
+
+    public TrackerHandler(AppCompatActivity appCompatActivity) {
+        super(appCompatActivity);
+        routes = retrofit.create(Tracker.class);
+    }
+    public void getTrackerList(Context context, Callback<ResponseBody> callBack){
+        Call<ResponseBody> call = routes.getTrackerList(SessionUtil.getHeaderAuthorization(context));
+        call.enqueue(callBack);
+    }
+    public void saveTracker(Callback<ResponseBody> callBack, String bean, Context context){
+        Call<ResponseBody> call = routes.savetTracker(SessionUtil.getHeaderAuthorization(context),bean);
+        call.enqueue(callBack);
+    }
+    public void getTrackerById(Callback<ResponseBody> callBack,String id,Context context){
+        Call<ResponseBody> call = routes.getTracker(SessionUtil.getHeaderAuthorization(context),id);
+        call.enqueue(callBack);
+    }
+    public void updateTrackerById(Callback<ResponseBody> callBack,String trackerId,String bean,Context context){
+        Call<ResponseBody> call = routes.updateTracker(SessionUtil.getHeaderAuthorization(context),trackerId,bean);
+        call.enqueue(callBack);
+    }
+    public void deleteTracker(Callback<ResponseBody> callBack,String trackerId,Context context){
+        Call<ResponseBody> call = routes.deleteTracker(SessionUtil.getHeaderAuthorization(context),trackerId);
+        call.enqueue(callBack);
+    }
+    public void getTrackerAvailability(Callback<ResponseBody> callBack,String id,Context context){
+        Call<ResponseBody> call = routes.getAvailabilityTracker(SessionUtil.getHeaderAuthorization(context),id);
+        call.enqueue(callBack);
+    }
 }
